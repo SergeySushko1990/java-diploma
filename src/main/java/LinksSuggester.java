4426a98713cb47a64f46e3ed2850111d5615ae21
SergeyS1990 <ale-pe@yandex.com>
2022-12-17 22:32:20 +0300

    Курсовая работа по Java.

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,13 +1,40 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinksSuggester {
 
+    private List<Suggest> suggestList = new ArrayList<>();
+
+    public List<Suggest> getSuggestList() {
+        return suggestList;
+    }
+
     public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line = bufferedReader.readLine();
+        StringBuilder text = new StringBuilder();
+        while (line != null) {
+            text.append(line).append("\n");
+            line = bufferedReader.readLine();
+        }
+        suggestList = suggest(text.toString());
 
     }
 
     public List<Suggest> suggest(String text) {
-        return null;
+
+        String[] lines = text.split("\n");
+        List<Suggest> suggestList = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] t = line.split("\t");
+            if (t.length != 3) {
+                throw new WrongLinksFormatException("Wrong number of arguments " + line);
+            }
+            suggestList.add(new Suggest(t[0], t[1], t[2]));
+        }
+        return suggestList;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,83 @@
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.annot.PdfLinkAnnotation;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Link;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // создаём конфиг
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
+        List<Suggest> suggestList = linksSuggester.getSuggestList();
 
         // перебираем пдфки в data/pdfs
+        var dir = new File("data/pdfs");
+        ArrayList<File> files = new ArrayList<>(Arrays.asList(Objects.requireNonNull(dir.listFiles())));
 
         // для каждой пдфки создаём новую в data/converted
+        for (var file : files) {
+            var doc = new PdfDocument(new PdfReader(file),
+                    new PdfWriter(new File("data/converted/" + file.getName()))
+            );
 
-        // перебираем страницы pdf
+            // перебираем страницы pdf
+            ArrayList<String> pgs = new ArrayList<>();
+            for (int i = 1; i < doc.getNumberOfPages(); i++) {
+                pgs.add(PdfTextExtractor.getTextFromPage(doc.getPage(i)));
 
-        // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
 
-        // вставляем туда рекомендуемые ссылки из конфига
+                List<String> keyWords = new ArrayList<>();
+                for (String page : pgs) {
+
+                    List<String> urls = new ArrayList<>();
+                    List<String> titles = new ArrayList<>();
+
+                    for (Suggest suggest : suggestList) {
+                        if (page.toLowerCase().contains(suggest.getKeyWord().toLowerCase()) &&
+                                !keyWords.contains(suggest.getKeyWord())) {
+                            keyWords.add(suggest.getKeyWord());
+                            urls.add(suggest.getUrl());
+                            titles.add(suggest.getTitle());
+                        }
+                    }
+                    // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
+                    if (urls.size() > 0) {
+                        var newPage = doc.addNewPage(page.indexOf(page) + i + 1);
+                        i++;
+                        var rect = new Rectangle(newPage.getPageSize()).moveRight(10).moveDown(10);
+                        Canvas canvas = new Canvas(newPage, rect);
+                        Paragraph paragraph = new Paragraph("Suggestions:\n");
+                        paragraph.setFontSize(25);
+
+                        // вставляем туда рекомендуемые ссылки из конфига
+                        for (int j = 0; j < urls.size(); j++) {
+                            PdfLinkAnnotation annotation = new PdfLinkAnnotation(rect);
+                            PdfAction action = PdfAction.createURI(urls.get(j));
+                            annotation.setAction(action);
+                            Link link = new Link(titles.get(j), annotation);
+                            paragraph.add(link.setUnderline());
+                            paragraph.add("\n");
+                        }
+
+                        canvas.add(paragraph);
+                        canvas.close();
+                    }
+                }
+            }
+            doc.close();
+        }
+        System.out.println("Files converted");
     }
 }
+
